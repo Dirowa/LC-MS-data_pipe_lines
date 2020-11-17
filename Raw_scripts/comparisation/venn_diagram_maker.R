b886de7184e1f14830951ff4f4341f4b16fc4ea0
Dirowa <62692537+Dirowa@users.noreply.github.com>
2020-11-17 21:11:32 +0100

    Update venn_diagram_maker.R

--- Raw_scripts/comparisation/venn_diagram_maker.R
+++ Raw_scripts/comparisation/venn_diagram_maker.R
@@ -1,10 +1,26 @@
 
-path <- "F:/avans/stage MM/peakpicking_output/venn_diagram_data.csv"
+install.packages('eulerr')
+library("eulerr")
+
+items <- c(1,3,4,7,8)
+
+
+  
+  
+path <- paste0("C:/Users/DVrin/OneDrive/Documenten/avans/stage_MM/peakpicking comparisation/_venn_diagram_data_",8,".csv")
 data_frame <- read.csv(path)
 data_frame <- data_frame[,-1]
-
-
-plot(euler(data_frame), counts = T, fontface = 1)
-
-
+  
+  
+plot(euler(data_frame),
+     #fills = TRUE,
+     #edges = TRUE,
+     legend = TRUE,
+     labels = identical(legend, TRUE),
+     quantities = TRUE,
+     strips = NULL,
+     main = NULL,
+     n = 200L,
+     adjust_labels = TRUE,
+)
 
