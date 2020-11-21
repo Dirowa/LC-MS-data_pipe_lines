6a44b096a581e8a855cc33c45adcf79b57566969
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2020-11-21 14:10:04 +0100

    Update Mayority_vote_calc.py

--- Raw_scripts/comparisation/Mayority_vote_calc.py
+++ Raw_scripts/comparisation/Mayority_vote_calc.py
@@ -1,9 +1,3 @@
-################################################################
-# NOTE ALWAYS CHECK IF DATA IS DUPLICATE BY CHANGE OR IT WILL#
-# Greatly give advance to some algorithms#
-#############################################
-
-
 ###############################################################
 # calculates the mayority vote from the venn diagram data csv##
 ###############################################################
@@ -42,37 +36,38 @@ for file in files:
     print('############################')
     print(file)
     with open(file1,'r') as f:
-        # read header and create a dictionary#
+        #read header and create a dictionary#
         header = (f.readline()).rstrip()
         header = (header.split(','))[1:]
         Dict = {el:0 for el in header}
-
+        print(Dict)
+    with open(file1, 'r') as f:
+        next(f)
         for line in f:
             TMP_line = line.rstrip()
             TMP_line = (TMP_line.split(','))[1:]
-            #print(TMP_line)
+            print(TMP_line)
             TMP_value = 0
             for item in TMP_line:
                 #print(item)
                 TMP_value = TMP_value + int(item)
 
+            print(TMP_value)
             # if there isnt an agreed item then the line will be dicsontinued
-            if TMP_value  == 1:
-                break
-
-            #print(TMP_line)
-            counter = -1
-            for number in TMP_line:
-                counter += 1
-                if int(number) != 0:
-                    TMP_header = (header[counter])
-                    Dict[TMP_header] += 1
+            if TMP_value  != 1:
+                #print(TMP_line)
+                counter = -1
+                for number in TMP_line:
+                    counter += 1
+                    if int(number) != 0:
+                        TMP_header = (header[counter])
+                        Dict[TMP_header] += 1
 
     Dict = {k: v for k, v in sorted(Dict.items(), key=lambda item: item[1])}
     for x,y in Dict.items():
         column = 0
         row+= 1
-        print(x,y)
+        #print(x,y)
         sheet.write(row, column, x)
         column += 1
         sheet.write(row, column, y)
