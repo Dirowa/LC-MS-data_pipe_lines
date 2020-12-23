04fddd847ab727187767f9d597fdd9fab29bceec
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2020-12-23 22:40:28 +0100

    Update Batch Correction.r
    there were a few bugs in data not being copied properly. thereby updated the files for the selection of QC files

--- Raw_scripts/Batch Correction.r
+++ Raw_scripts/Batch Correction.r
@@ -1,35 +1,35 @@
 ########################################################
 # installing softawre#############
 ##########################################
-BiocManager::install("ProteoMM")
-library(ProteoMM)
-data("hs_peptides") # loads variable hs_peptides
-dim(hs_peptides)  # 695 x 13
-remotes::install_github("ricoderks/Rcpm")
-library(Rcpm)
-test_check("Rcpm")
+#BiocManager::install("ProteoMM")
+#library(ProteoMM)
+#data("hs_peptides") # loads variable hs_peptides
+#dim(hs_peptides)  # 695 x 13
+#remotes::install_github("ricoderks/Rcpm")
+#library(Rcpm)
+#test_check("Rcpm")
 
-metanr_packages <- function(){
-  metr_pkgs <- c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest","RBGL","edgeR","fgsea","devtools","crmn")
-  list_installed <- installed.packages()
-  new_pkgs <- subset(metr_pkgs, !(metr_pkgs %in% list_installed[, "Package"]))
-  if(length(new_pkgs)!=0){if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-    BiocManager::install(new_pkgs)
-    print(c(new_pkgs, " packages added..."))
-  }
-  
-  if((length(new_pkgs)<1)){
-    print("No new packages added...")
-  }
-}
-metanr_packages()
+#metanr_packages <- function(){
+#  metr_pkgs <- c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest","RBGL","edgeR","fgsea","devtools","crmn")
+#  list_installed <- installed.packages()
+#  new_pkgs <- subset(metr_pkgs, !(metr_pkgs %in% list_installed[, "Package"]))
+#  if(length(new_pkgs)!=0){if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#    BiocManager::install(new_pkgs)
+#    print(c(new_pkgs, " packages added..."))
+#  }
+#  
+#  if((length(new_pkgs)<1)){
+#    print("No new packages added...")
+#  }
+#}
+#metanr_packages()
 
 
 
 
 library(colSu)
-install.packages("pacman")
+#install.packages("pacman")
 
 library(pacman)
 library("impute")
@@ -52,13 +52,13 @@ library("fgsea")
 library(MetaboAnalystR)
 library(dplyr)
 
-install.packages("devtools")
+
 library(devtools)
 library( phenomis)
 
 
 # Step 2: Install MetaboAnalystR with documentation
-devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = TRUE, build_manual =T, force = TRUE)
+#devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = TRUE, build_manual =T, force = TRUE)
 
 ########################
 # important variables #
@@ -119,12 +119,7 @@ intensities <- intensities[,-1]
 # combining#
 ###############\
 
-combined <- rbind(metadata1[2,], intensities[-1,])
-
-
-###########################
-# change all NA data in 0##
-###########################
+combined <- rbind(metadata1[2,], intensities)
 combined[is.na(combined)] <- 0
 write.csv(combined, 'BatchCorrect_import_data_1')
 
@@ -154,7 +149,12 @@ ordered_normalized_set <- normalized_set[order(row.names(normalized_set)), ]
 # ordering datasets #
 #####################
 meta_data <- read.csv('batchCorrect_import_metadata')
-new_normalized_set <- cbind(meta_data[,2:4], ordered_normalized_set);
+new_normalized_set <- cbind(meta_data[,2:5], ordered_normalized_set);
+new_normalized_set <- new_normalized_set[,-1]
+
+new_normalized_set$sampleType <- as.character(new_normalized_set$sampleType)
+new_normalized_set$sampleType[new_normalized_set$sampleType == QC] <- "QC"
+
 write.csv(new_normalized_set, "new_normalized_set.csv")
 
 ################
@@ -164,21 +164,21 @@ mSet <- PCA.Anal(mSet)
 mSet <- PlotPCAPairSummary(mSet, "pca_pair_0_", "png", 72, width=NA, 5)
 mSet <- PlotPCAScree(mSet, "pca_scree_0_", "png", 72, width=NA, 5)
 mSet <- PlotPCA2DScore(mSet, "pca_score2d_0_", "png", 72, width=NA, 1,2,0.95,0,0)
-rm(mSet)
+
 
 
 
 #############################
 # performing batchcorrection#
 #############################
-mSet <- InitDataObjects("pktable", "utils", FALSE)
-mSet <- Read.BatchDataTB(mSet, "new_normalized_set.csv", "row")
-methods<- c("Loess", "Combat", "WaveICA","EigenMS","QC_RLSC","ANCOVA","RUV_random","RUV_2","RUV_s","RUV_r","RUV_g","NOMIS","CCMN")
+rm(mSet)
+#mSet <- InitDataObjects("pktable", "utils", FALSE)
+#mSet <- Read.BatchDataTB(mSet, "new_normalized_set.csv", "row")
+methods<- c("Loess", "Combat", "WaveICA","EigenMS","QC_RLSC","ANCOVA")
 counter <- 0
 items <- list()
 
 
-# try function becouse not all datasets accepts the other functions (to many arguments for the LOG2 function)
 for (i in 1:length(methods))  {
   counter <- counter + 1
   if (methods[i] == "Loess"){
@@ -186,7 +186,6 @@ for (i in 1:length(methods))  {
                              files.ls = list(dataMatrix = file.path(path_to_Data_matrix_xcms),
                                              sampleMetadata = file.path(path_to_samplemetadata),
                                              variableMetadata = file.path(Path_to_variable_metadata)))
-    
     
     corrcted_set <- correcting(
       eset,
@@ -201,29 +200,30 @@ for (i in 1:length(methods))  {
     )
     corrcted_set <- inspecting(corrcted_set)
     corrcted_set <- phenomis::transforming(corrcted_set, method.c = "log10")
-    phenomis::writing(corrcted_set, dir.c = "F:/avans/stage MM/Sherloktest_data_2/peakpick_output", prefix.c = 'loessALL',
+    phenomis::writing(corrcted_set, dir.c = output, prefix.c = 'loessALL',
                       overwrite.l = TRUE)
     #filter for only QC sets
     corrcted_set <- corrcted_set[, Biobase::pData(corrcted_set)[, "sampleType"] == QC]
-    
-    
-    phenomis::writing(corrcted_set, dir.c = "F:/avans/stage MM/Sherloktest_data_2/peakpick_output", prefix.c = 'loessQC',
+    phenomis::writing(corrcted_set, dir.c = output, prefix.c = 'loessQC_BatchCorrected_data',
                       overwrite.l = TRUE)
-    items[[counter]] <- 'loessQC_dataMatrix.tsv'
+    
+    file.remove(paste0(output,'/',"loessQC_BatchCorrected_data_sampleMetadata.tsv"))
+    file.remove(paste0(output,'/',"loessQC_BatchCorrected_data_variableMetadata.tsv"))
     
   }
   else {
-  try(mSet <- PerformBatchCorrection(mSetObj = mSet, imgName = methods[[i]], Method = methods[[i]]), silent = T)
-  (info <- (mSet$dataSet$interbatch_dis))
+  rm(mSet)
+  mSet <- InitDataObjects("pktable", "utils", FALSE)
+  mSet <- Read.BatchDataTB(mSet, "new_normalized_set.csv", "row")
+  try(mSet <- PerformBatchCorrection(mSetObj = mSet, imgName = methods[[i]], Method = methods[[i]]), silent = T) 
+  #try(info <- (mSet$dataSet$interbatch_dis))
+  safe_name <- paste0("BatchCorrected_data",'_',methods[[i]],'.csv')
+  (file.rename("MetaboAnalyst_batch_data.csv", safe_name))
   
-  safe_name <- paste0("MetaboAnalyst_batch_data.csv",'_',methods[[i]],'.csv')
-  file.rename("MetaboAnalyst_batch_data.csv", safe_name)
-  items[[counter]] <- safe_name
   }
 }
 
 
-items[[(counter + 1)]] <- "new_normalized_set.csv"
 
 if (counter == 0){
   print('NO batch correction worked')
@@ -234,44 +234,54 @@ if (counter == 0){
 ###############################################
 # calculate average variance across QC samples#
 ###############################################
+items <- list.files(path = output, pattern = "BatchCorrected_data", all.files = FALSE,
+                    full.names = FALSE, recursive = FALSE,
+                    ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 
+items[[(length(items) + 1)]] <- "new_normalized_set.csv"
+
+
+items
 means <- list()
 for (i in 1:length(items)){
   items <- Filter(Negate(is.null), items)
+  try(rm(variance_dataframe))
   print(items[[i]])
   if (items[[i]] == "new_normalized_set.csv"){
     variance_dataframe <- read.csv(paste0(output,'/','new_normalized_set.csv'), header = TRUE, sep = ',')
-    variance_dataframe <- variance_dataframe[grep(QC, variance_dataframe$sampleType),]
+    variance_dataframe <- variance_dataframe[grep('QC', variance_dataframe$sampleType),]
     variance_dataframe <- variance_dataframe[,-1:-4]
     #variance_dataframe[is.na(variance_dataframe)] <- 0
     means[[i]] <- mean(rowVars(variance_dataframe))
+    rm(variance_dataframe)
 
-    
   }
  
   
-  else if (as.character(items[[i]]) == 'loessQC_dataMatrix.tsv'){
+  else if (as.character(items[[i]]) == 'loessQC_BatchCorrected_data_dataMatrix.tsv'){
 
-    variance_dataframe <- read.csv(paste0(output,'/',"loessQC_dataMatrix.tsv"), header = FALSE, sep = '\t')
+    variance_dataframe <- read.csv(paste0(output,'/',"loessQC_BatchCorrected_data_dataMatrix.tsv"), header = FALSE, sep = '\t')
     variance_dataframe <- as.data.frame(t(variance_dataframe))
     variance_dataframe <- variance_dataframe[,-1]
     variance_dataframe <- variance_dataframe[-1,]
     variance_dataframe <- na.exclude(variance_dataframe)
     variance_dataframe <- sapply(variance_dataframe,  var)
     means[[i]] <- mean(variance_dataframe)
+    rm(variance_dataframe)
   }
   else {
-    print(items[[i]])
-    variance_dataframe <- read.csv(paste0(output,'/',items[[4]]), header = TRUE, sep = ',')
-    variance_dataframe <- variance_dataframe[grep(QC, variance_dataframe$Dataset),]
+    variance_dataframe <- read.csv(paste0(output,'/',items[[i]]), header = TRUE, sep = ',')
+    variance_dataframe <- variance_dataframe[grep("QC", variance_dataframe$CLASS),]
     variance_dataframe <- variance_dataframe[,-1:-3]
-    class(variance_dataframe)
     #variance_dataframe[is.na(variance_dataframe)] <- 0
     variance_dataframe <- sapply(variance_dataframe,  var)
     means[[i]] <- mean(variance_dataframe)
+    rm(variance_dataframe)
   }
 }
 
+
+## ordering output
 means1 <- do.call(rbind, Map(data.frame, mean=means, file=items))
 means <- means1[order(means1$mean),]
 print(means)
@@ -280,7 +290,7 @@ write.csv(means, file = 'Benchmarking_batch_correction.csv')
 
 
 #selecting Best dataset
-best <- means[1,2]
+
 
 if (sub('.*\\.', '', means[1,2]) == "tsv"){
   try(means <- gsub('QC', 'ALL',means[1,2]))
@@ -289,15 +299,15 @@ if (sub('.*\\.', '', means[1,2]) == "tsv"){
   rownames(best_batch_corrected) <- best_batch_corrected[,1]
   best_batch_corrected <- best_batch_corrected[,-1]
   
-  write.csv(best_batch_corrected,'Best_batchcorrected.csv')
+  write.csv(best_batch_corrected,'Best_batchcorrected1.csv')
   
   
 } else if ((sub('.*\\.', '', means[1,2]) == "csv") ) {
-  best_batch_corrected <- read.csv(means[2,2])
-  write.csv(best_batch_corrected,'Best_batchcorrected.csv')
+  best_batch_corrected <- read.csv(means[1,2])
   best_batch_corrected <- as.data.frame((best_batch_corrected))
   rownames(best_batch_corrected) <- best_batch_corrected[,1]
-  best_batch_corrected <- best_batch_corrected[,-1:-3]
+  best_batch_corrected <- t(best_batch_corrected[,-1:-3])
+  
   write.csv(best_batch_corrected,'Best_batchcorrected.csv')
 
 }
@@ -319,6 +329,5 @@ mSet <- PCA.Anal(mSet)
 mSet <- PlotPCAPairSummary(mSet, "pca_Best_corrected_pair_0_", "png", 72, width=NA, 5)
 mSet <- PlotPCAScree(mSet, "pca__Best_corrected_scree_0_", "png", 72, width=NA, 5)
 mSet <- PlotPCA2DScore(mSet, "pca_Best_corrected__score2d_0_", "png", 72, width=NA,  1,2,0.95,0,1)
-
 
 
